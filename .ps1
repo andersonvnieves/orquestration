6fab4aa78e2560157e3ac61c99fd6113aa1f65ee
Anderson Nieves <andersonvnieves@live.com>
2026-06-24 23:55:20 -0300

    Criação do docker.compose

---
+++ .ps1
@@ -0,0 +1,6 @@
+docker run -d --name rabbitmq -p 5672:5672 -p 15672:15672 -e RABBITMQ_DEFAULT_USER=admin -e RABBITMQ_DEFAULT_PASS=admin123 rabbitmq:management
+
+
+docker compose up --build
+
+docker compose up
